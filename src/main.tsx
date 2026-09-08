938731dc37c2a3773d09061c509066ab862ab287
Nuno Colaço <nuno.colaco96@gmail.com>
2026-09-08 23:00:51 +0100

    Change urls, redirect and canonical url

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,20 @@ import './styles/index.css'
 import App from './app/App.tsx'
 import { LanguageProvider } from './i18n/LanguageContext.tsx'
 import { CookieConsentProvider } from './i18n/CookieConsentContext.tsx'
+import { getCanonicalRedirectUrl } from './lib/seo'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <LanguageProvider>
-      <CookieConsentProvider>
-        <App />
-      </CookieConsentProvider>
-    </LanguageProvider>
-  </StrictMode>,
-)
+const canonicalRedirect = getCanonicalRedirectUrl(window.location)
+
+if (canonicalRedirect) {
+  window.location.replace(canonicalRedirect)
+} else {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <LanguageProvider>
+        <CookieConsentProvider>
+          <App />
+        </CookieConsentProvider>
+      </LanguageProvider>
+    </StrictMode>,
+  )
+}
